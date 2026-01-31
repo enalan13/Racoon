ef062580e2ef1dd06d2047ea7f1126c9c8fa2fee
senayagmur <yagmurcandik@gmail.com>
2026-01-31 05:13:46 -0500

    its gone

--- fastapi_test/main.py
+++ fastapi_test/main.py
@@ -1,20 +1,20 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.templating import Jinja2Templates
-#sena  = here
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-
-items = []
-
-@app.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request, "items": items}
-    )
-
-@app.post("/add")
-def add_item(item: str = Form(...)):
-    items.append(item)
-    return RedirectResponse(url="/", status_code=303)
+# from fastapi import FastAPI, Request, Form
+# from fastapi.responses import HTMLResponse, RedirectResponse
+# from fastapi.templating import Jinja2Templates
+# #sena  = here
+# app = FastAPI()
+# templates = Jinja2Templates(directory="templates")
+#
+# items = []
+#
+# @app.get("/", response_class=HTMLResponse)
+# def home(request: Request):
+#     return templates.TemplateResponse(
+#         "index.html",
+#         {"request": request, "items": items}
+#     )
+#
+# @app.post("/add")
+# def add_item(item: str = Form(...)):
+#     items.append(item)
+#     return RedirectResponse(url="/", status_code=303)
